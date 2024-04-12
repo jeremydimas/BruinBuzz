ccb42111ce729cf4c5c092bb1d326e3b9322f2f6
Jeremy Dimas <jeremydimas10@gmail.com>
2024-04-12 11:41:10 -0700

    Initial Commit

---
+++ BruinBuzz/BruinBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BruinBuzzApp.swift
+//  BruinBuzz
+//
+//  Created by Jeremy Dimas on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BruinBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BruinBuzz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BruinBuzz
+//
+//  Created by Jeremy Dimas on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
